6ead67c6afb240e5aadb9018c9100cb681decefd
QiaoJiancheng <124369783+QiaoJiancheng@users.noreply.github.com>
2023-02-04 15:33:24 +0800

    pull commit

--- cloud-api-commons/src/main/java/com/atguigu/springcloud/test/PushTest.java
+++ cloud-api-commons/src/main/java/com/atguigu/springcloud/test/PushTest.java
@@ -3,5 +3,6 @@ package com.atguigu.springcloud.test;
 public class PushTest {
     public static void main(String[] args) {
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
